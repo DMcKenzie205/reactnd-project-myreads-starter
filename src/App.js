43afecf54645a46aed76b37f25fc8ba440a10f8b
Daniel McKenzie <d.mckenzie205@gmail.com>
2018-11-11 17:36:16 +0100

    local state manage for shelfChanger

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import * as BooksAPI from './BooksAPI';
 import BookCase from './components/BookCase';
 import { Route } from 'react-router-dom';
+import { Switch } from 'react-router'
 import Search from './components/search';
 
 import './App.css'
@@ -22,10 +23,19 @@ class BooksApp extends React.Component {
 
     // Helper function for changing the shelf a book is assigned to
     changeShelf = (book, shelf) => {
-        BooksAPI.update(book, shelf).then(() => {
-            BooksAPI.getAll().then((books) => {
-                this.setState({ books: books })
+        BooksAPI.update(book, shelf).then(res => {
+            // Loop through the updated shelves and filter the 'shelf'
+            // that includes the selected book id
+            const newShelf = Object.keys(res).filter(shelf => res[shelf].includes(book.id))[0];
+            const newBooks = this.state.books.map(b => {
+                if (b.id === book.id) {
+                    b.shelf = newShelf;
+                }
+
+                return b;
             })
+
+            this.setState({ books: newBooks });
         })
     }
 
@@ -33,18 +43,20 @@ class BooksApp extends React.Component {
 
         return (
             <div className="app">
-                <Route exact path='/' render={() => (
-                    <BookCase 
-                        books={this.state.books} 
-                        changeShelf={this.changeShelf}
-                    />
-                )}/>
-                <Route path='/search' render={() => (
-                    <Search 
-                        books={this.state.books} 
-                        changeShelf={this.changeShelf}
-                    />
-                )}/>  
+                <Switch>
+                    <Route exact path='/' render={() => (
+                        <BookCase 
+                            books={this.state.books} 
+                            changeShelf={this.changeShelf}
+                        />
+                    )}/>
+                    <Route path='/search' render={() => (
+                        <Search 
+                            books={this.state.books} 
+                            changeShelf={this.changeShelf}
+                        />
+                    )}/> 
+                </Switch> 
             </div>
 
         )
